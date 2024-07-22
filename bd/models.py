36005503ede88ba27ce6e7de625f33d596ea66c8
maciejkubiczekgum <maciej.kubiczek@gum.gov.pl>
2024-07-22 09:21:45 +0200

    test

---
+++ bd/models.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def add(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
